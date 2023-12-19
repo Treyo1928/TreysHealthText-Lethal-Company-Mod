ef9eada7e7dbd092207912d1fecb5a0d14fcf29f
Trey <85683709+Treyo1928@users.noreply.github.com>
2023-12-19 14:58:52 -0600

    Update HUDManager_UpdateHealthUI_Patch.cs
    Fixed bug where HealthText would scale with weight because they shared a parent.

--- HUDManager_UpdateHealthUI_Patch.cs
+++ HUDManager_UpdateHealthUI_Patch.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using TMPro;
 using HarmonyLib;
 
@@ -7,47 +7,88 @@ namespace TreysHealthText
     [HarmonyPatch(typeof(HUDManager), "UpdateHealthUI")]
     public static class HUDManager_UpdateHealthUI_Patch
     {
+        private static TextMeshProUGUI healthText = null;
+
         static void Postfix(HUDManager __instance, int health)
         {
-            // Find or create the TextMeshPro object
-            TextMeshProUGUI healthText = GameObject.Find("PlayerHealthText")?.GetComponent<TextMeshProUGUI>();
             if (healthText == null)
             {
-                // Use the weightCounter's parent as the parent for the new text object
-                Transform parentTransform = __instance.weightCounter.transform.parent;
-
-                // Create a new GameObject for the TextMeshPro element
-                GameObject textObj = new GameObject("PlayerHealthText");
-                textObj.transform.SetParent(parentTransform, false);
-
-                // Add and configure the TextMeshPro component
-                healthText = textObj.AddComponent<TextMeshProUGUI>();
-
-                // Clone properties from weightCounter
-                TextMeshProUGUI referenceText = __instance.weightCounter;
-                healthText.font = referenceText.font;
-                healthText.fontSize = referenceText.fontSize;
-                healthText.color = referenceText.color;
-                healthText.alignment = TextAlignmentOptions.Center; // Center alignment
-                healthText.enableAutoSizing = referenceText.enableAutoSizing;
-                healthText.fontSizeMin = referenceText.fontSizeMin;
-                healthText.fontSizeMax = referenceText.fontSizeMax;
-
-                // Adjust position relative to the new parent
-                RectTransform rectTransform = textObj.GetComponent<RectTransform>();
-                rectTransform.anchoredPosition = new Vector2(-82, 25); // Adjust position as needed
-
-                Plugin.PluginLogger.LogInfo("Health Text Asset Created.");
+                CreateHealthText(__instance, 100); // Initial health value
             }
 
-            // Update the text to show the current health
             healthText.text = $"HP\n{health}";
 
-            // Destroy the text object if the player's health is 0 or less
             if (health <= 0)
             {
                 GameObject.Destroy(healthText.gameObject);
+                healthText = null;
             }
+        }
+
+        private static void CreateHealthText(HUDManager hudManager, int initialHealth)
+        {
+            // Find the 'TopLeftCorner' GameObject (parent of 'Self')
+            GameObject topLeftCorner = GameObject.Find("Systems/UI/Canvas/IngamePlayerHUD/TopLeftCorner");
+
+            if (topLeftCorner == null)
+            {
+                Plugin.PluginLogger.LogError("'TopLeftCorner' not found.");
+                return;
+            }
+
+            // Create a new GameObject for the TextMeshPro element
+            GameObject textObj = new GameObject("PlayerHealthText");
+            textObj.transform.SetParent(topLeftCorner.transform, false); // Parent to 'TopLeftCorner'
+
+            // Add and configure the TextMeshPro component
+            healthText = textObj.AddComponent<TextMeshProUGUI>();
+
+            // Clone properties from weightCounter (if available)
+            if (hudManager.weightCounter != null)
+            {
+                TextMeshProUGUI weightText = hudManager.weightCounter;
+                healthText.font = weightText.font;
+                healthText.fontSize = weightText.fontSize;
+                healthText.color = weightText.color;
+                healthText.alignment = TextAlignmentOptions.Center;
+                healthText.enableAutoSizing = weightText.enableAutoSizing;
+                healthText.fontSizeMin = weightText.fontSizeMin;
+                healthText.fontSizeMax = weightText.fontSizeMax;
+
+                // Clone material properties
+                if (weightText.fontMaterial != null)
+                {
+                    healthText.fontSharedMaterial = new Material(weightText.fontMaterial);
+                }
+
+                // Apply rotation from weightCounter's parent to healthText
+                if (weightText.transform.parent != null)
+                {
+                    RectTransform weightCounterParentRect = weightText.transform.parent.GetComponent<RectTransform>();
+                    if (weightCounterParentRect != null)
+                    {
+                        RectTransform healthTextRect = healthText.GetComponent<RectTransform>();
+                        healthTextRect.localRotation = weightCounterParentRect.localRotation;
+                    }
+                }
+            }
+            else
+            {
+                // Set default properties if weightCounter is not available
+                healthText.fontSize = 24; // Adjust as needed
+                healthText.color = Color.red; // Default color
+                healthText.alignment = TextAlignmentOptions.Center; // Center alignment
+            }
+
+            // Configure RectTransform for positioning
+            RectTransform rectTransform = textObj.GetComponent<RectTransform>();
+            rectTransform.anchorMin = new Vector2(0, 1); // Top left anchor
+            rectTransform.anchorMax = new Vector2(0, 1); // Top left anchor
+            rectTransform.pivot = new Vector2(0, 1); // Top left pivot
+            rectTransform.anchoredPosition = new Vector2(-53, -95); // Adjust position as needed
+
+            // Set initial health text
+            healthText.text = $"HP\n{initialHealth}";
         }
     }
 }
